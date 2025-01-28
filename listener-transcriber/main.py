9b287983e88020b8be884e348569a54ed1fb29ee
maciej_ciukaj <maciek.ciukaj@gmail.com>
2025-01-28 19:47:46 +0100

    Example recording added for transcription purposes

--- listener-transcriber/main.py
+++ listener-transcriber/main.py
@@ -1,4 +1,6 @@
+from dotenv import load_dotenv
 from Services.transcription_service import transcription_service
 
-print(transcription_service("sample_1"))
+load_dotenv()
+transcription_service("E06_2024-12-10T15_31_55Z_12139.0kHz")
 
